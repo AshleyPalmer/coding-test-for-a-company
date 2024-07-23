30febcf4533b817a39a0645d0e6cd0cb5787b8e7
Ashley Palmer <ashley.palmer@eckoh.com>
2024-07-23 20:35:44 +0100

    Investor object setup with getters and setters

--- src/Domain/Investor/Investor.php
+++ src/Domain/Investor/Investor.php
@@ -4,4 +4,82 @@ namespace LendInvest\Domain;
 
 class Investor
 {
+    protected int $id;
+    protected string $investorName;
+    protected Wallet $wallet;
+    //TOOD: Add Loan associations
+
+    public function __construct(
+        int $id,
+        string $investorName,
+        Wallet $wallet
+    ) {
+        $this->id = $id;
+        $this->investorName = $investorName;
+        $this->wallet = $wallet;
+    }
+
+    /**
+     * Get Investor ID
+     * 
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Investor ID
+     * @param int $id
+     * @return Investor
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get Investor Name
+     * @return string
+     */
+    public function getInvestorName(): string
+    {
+        return $this->investorName;
+    }
+
+    /**
+     * Set Investor Name
+     * @param string $investorName
+     * @return Investor
+     */
+    public function setInvestorName(string $investorName): self
+    {
+        $this->investorName = $investorName;
+
+        return $this;
+    }
+
+    /**
+     * Get Investor's Wallet
+     * @return Wallet
+     */
+    public function getWallet(): Wallet
+    {
+        return $this->wallet;
+    }
+
+    /**
+     * Set Investor's Wallet
+     * @param Wallet $wallet
+     * @return Investor
+     */
+    public function setWallet(Wallet $wallet): self
+    {
+        $this->wallet = $wallet;
+
+        return $this;
+    }
 }
