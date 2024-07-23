25fc4d89fcaed5b2de432034091f4946bf74d889
Ashley Palmer <ashley.palmer@eckoh.com>
2024-07-23 20:36:41 +0100

    Tranche object setup with getters and setters

--- src/Domain/Tranche/Tranche.php
+++ src/Domain/Tranche/Tranche.php
@@ -2,6 +2,84 @@
 
 namespace LendInvest\Domain;
 
+use Money\Money;
+
 class Tranche
 {
+    protected string $name;
+    protected int $interestRate;
+    protected Money $availableInvestment;
+
+    public function __construct(
+        string $name,
+        int $interestRate,
+        Money $availableInvestment
+    ) {
+        $this->name = $name;
+        $this->interestRate = $interestRate;
+        $this->availableInvestment = $availableInvestment;
+    }
+
+    /**
+     * Get Tranche Name
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set Tranche Name
+     * @param string $name
+     * @return Tranche
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get Tranche Interest Rate
+     * @return int
+     */
+    public function getInterestRate(): int
+    {
+        return $this->interestRate;
+    }
+
+    /**
+     * Set Tranche Interest Rate
+     * @param int $interestRate
+     * @return Tranche
+     */
+    public function setInterestRate(int $interestRate): self
+    {
+        $this->interestRate = $interestRate;
+
+        return $this;
+    }
+
+    /**
+     * Get Available Investment Amount
+     * @return Money
+     */
+    public function getAvailableInvestment(): Money
+    {
+        return $this->availableInvestment;
+    }
+
+    /**
+     * Set Available Investment Amount
+     * @param Money $amount
+     * @return Tranche
+     */
+    public function setAvailableInvestment(Money $amount): self
+    {
+        $this->availableInvestment = $amount;
+
+        return $this;
+    }
 }
