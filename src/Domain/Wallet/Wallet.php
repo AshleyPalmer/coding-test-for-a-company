7e3aec8792dd73e82bb69d0f6026c0714e86a3b6
Ashley Palmer <ashley.palmer@eckoh.com>
2024-07-23 20:37:39 +0100

    Wallet object setup with getters and setters

--- src/Domain/Wallet/Wallet.php
+++ src/Domain/Wallet/Wallet.php
@@ -2,6 +2,45 @@
 
 namespace LendInvest\Domain;
 
+use Money\Currency;
+use Money\Money as Money;
+
 class Wallet
 {
+    protected Money $amount;
+
+    public function __construct(string $amount)
+    {
+        $this->amount = new Money($amount, new Currency('GBP'));
+    }
+
+    /**
+     * Get Wallet Amount
+     * @return Money
+     */
+    public function getAmount(): Money
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set Wallet Amount
+     * @param Money $amount
+     * @return Wallet
+     */
+    public function setAmount(Money $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Returns Wallet Amount as a Money JSON object
+     * @return array
+     */
+    public function getAmountJson(): array
+    {
+        return $this->amount->jsonSerialize();
+    }
 }
