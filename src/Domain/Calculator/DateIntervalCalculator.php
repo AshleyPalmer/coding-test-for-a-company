e118bab52badaabf71592266ab62d4d2c16a03db
Ashley Palmer <ashley.palmer@eckoh.com>
2024-07-24 13:39:52 +0100

    Helpers for calculating interest rates and time intervals

---
+++ src/Domain/Calculator/DateIntervalCalculator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LendInvest\Domain;
+
+use DateTime;
+
+class DateIntervalCalculator
+{
+
+    /**
+     * Calculate the length of time invested in days
+     * 
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     * @return int
+     */
+    public function getInvestmentPeriodTerm(DateTime $startDate, DateTime $endDate): int
+    {
+        /**
+         * diff sum includes the start date but not the end date,
+         * so add a day onto the end date to include it in the sum.
+         */
+        $endDate->modify('+1 day');
+
+        return $startDate->diff($endDate)->format('%a');
+    }
+}
